853101381c07a9c86013b282ffe7769cd33a3094
Vlad Ploschyk <vlad.go.11199@gmail.com>
2020-03-01 14:26:17 +0200

    hw3

---
+++ module-3/task-6.js
@@ -0,0 +1,21 @@
+// Напиши функцию calculateTotalPrice(arr, productName), которая получает массив объектов
+//  и имя продукта (значение свойства name).
+// Возвращает общую стоимость продукта (цена * количество).
+
+function calculateTotalPrice(arr, productName) {
+  const totalPrice = 0;
+  for (const el of arr) {
+    if (el.name === productName) return el.price * el.quantity;
+  }
+}
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
